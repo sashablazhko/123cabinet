59427e4524327b3905746301921e95007c18cea3
Sasha Blazhko <sashablazhko@gmail.com>
2018-08-20 15:53:13 +0300

    srnding project to Evgen

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
-// select user start
+// select user in cabinet
 $('.dropdown__fio > .caption').on('click', function() {
 	$(this).parent().toggleClass('open');
 });
@@ -21,6 +21,68 @@ $(document).on('click', function(evt) {
 		$('.dropdown__fio').removeClass('open');
 	}
 });
-// select user end
+// select user in cabinet end
+
+// select city on addr page
+$('.dropdowncity > .caption').on('click', function() {
+	$(this).parent().toggleClass('open');
+});
+
+$('.dropdowncity > .list > .item').on('click', function() {
+	$('.dropdowncity > .list > .item').removeClass('selected');
+	$('.dropdowncity > .caption').addClass('caption__selected');
+	$(this).addClass('selected').parent().parent().removeClass('open').children('.caption').text( $(this).text() );
+});
+
+$(document).on('keyup', function(evt) {
+	if ( (evt.keyCode || evt.which) === 27 ) {
+		$('.dropdowncity').removeClass('open');
+	}
+});
+
+$(document).on('click', function(evt) {
+	if ( $(evt.target).closest(".dropdowncity > .caption").length === 0 ) {
+		$('.dropdowncity').removeClass('open');
+	}
+});
+// select city on addr page end
+
+// slider on password page (start)
+$('.passslider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+// slider on password page (end)
 
 });
